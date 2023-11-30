72e979642e5b4af66b4f56e2bf827ba914f9dc10
Mehintoz <152495874+Mehintoz@users.noreply.github.com>
2023-11-30 20:56:43 +0800

    Add GUI code
    For leave application form

--- src/ceu/LeaveApplicationFrame.java
+++ src/ceu/LeaveApplicationFrame.java
@@ -1,7 +1,22 @@
 package ceu;
 
-class LeaveApplicationFrame {
-	public static void main( String[] args ) {
-		
+import javax.swing.*;
+
+public static void main( String[] args ) {
+		JFrame frame = new JFrame ("Leave Application Form")
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,300);
+		JButton button = new JButton("Press");
+		frame.getContentPane().add(button); // Adds Button to content pane of frame
+		frame.setVisible(true);
+		}
+class LeaveApplicationFrame{
+	public static void main(String args[]){
+		JFrame frame = new JFrame("Leave Application Form");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,300);
+		JButton button1 = new JButton("Press");
+		frame.getContentPane().add(button1);
+		frame.setVisible(true);
 	}
 }
