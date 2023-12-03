8babd6c45b340e72aff57cbd8660d80b4e324050
Juice <j.q.natino@gmail.com>
2023-12-03 23:37:45 +0800

    Update AdminLeaveEndorsementGUI.java

--- src/ceu/AdminLeaveEndorsementGUI.java
+++ src/ceu/AdminLeaveEndorsementGUI.java
@@ -33,7 +33,6 @@ public class AdminLeaveEndorsementGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtDate;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -43,6 +42,7 @@ public class AdminLeaveEndorsementGUI extends JFrame {
 	private JTextField textField_6;
 	private JTextField textField_7;
 	private JTextField textField_8;
+	private JTextField textField_9;
 
 	/**
 	 * Launch the application.
@@ -92,181 +92,182 @@ public class AdminLeaveEndorsementGUI extends JFrame {
 		ApplicationForm.add(lblForm);
 		
 		JPanel panel = new JPanel();
+		panel.setLayout(null);
 		panel.setBounds(10, 23, 309, 453);
 		ApplicationForm.add(panel);
-		panel.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("Leave Application");
 		lblNewLabel_1.setVerticalAlignment(SwingConstants.BOTTOM);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 10));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 10));
 		lblNewLabel_1.setBounds(89, 21, 117, 23);
 		panel.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Center Scholar University");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblNewLabel_2.setBounds(70, 11, 154, 14);
 		panel.add(lblNewLabel_2);
 		
-		txtDate = new JTextField();
-		txtDate.setBounds(105, 56, 180, 20);
-		panel.add(txtDate);
-		txtDate.setColumns(10);
-		
-		JLabel lblNewLabel_3 = new JLabel("Leave Type:");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblNewLabel_3.setBounds(10, 80, 60, 14);
-		panel.add(lblNewLabel_3);
-		
 		JLabel lblNewLabel_4 = new JLabel("Date:");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblNewLabel_4.setBounds(10, 59, 46, 14);
+		lblNewLabel_4.setBounds(10, 78, 46, 14);
 		panel.add(lblNewLabel_4);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Vacation");
-		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnNewRadioButton.setBounds(70, 76, 66, 23);
-		panel.add(rdbtnNewRadioButton);
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(119, 75, 166, 20);
+		panel.add(textField);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Sick");
 		rdbtnNewRadioButton_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnNewRadioButton_1.setBounds(135, 76, 46, 23);
+		rdbtnNewRadioButton_1.setBounds(135, 95, 46, 23);
 		panel.add(rdbtnNewRadioButton_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Leave Type:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNewLabel_3.setBounds(10, 99, 60, 14);
+		panel.add(lblNewLabel_3);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Vacation");
+		rdbtnNewRadioButton.setSelected(true);
+		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		rdbtnNewRadioButton.setBounds(70, 95, 66, 23);
+		panel.add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Nuptial");
 		rdbtnNewRadioButton_2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnNewRadioButton_2.setBounds(183, 76, 55, 23);
+		rdbtnNewRadioButton_2.setBounds(183, 95, 55, 23);
 		panel.add(rdbtnNewRadioButton_2);
 		
 		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("Emergency");
 		rdbtnNewRadioButton_3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnNewRadioButton_3.setBounds(236, 76, 109, 23);
+		rdbtnNewRadioButton_3.setBounds(236, 95, 86, 23);
 		panel.add(rdbtnNewRadioButton_3);
-		
-		JRadioButton rdbtnPaternity = new JRadioButton("Paternity");
-		rdbtnPaternity.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnPaternity.setBounds(10, 95, 66, 23);
-		panel.add(rdbtnPaternity);
-		
-		JRadioButton rdbtnBirthdayLeave = new JRadioButton("Birthday Leave");
-		rdbtnBirthdayLeave.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnBirthdayLeave.setBounds(80, 95, 93, 23);
-		panel.add(rdbtnBirthdayLeave);
 		
 		JRadioButton rdbtnUnion = new JRadioButton("Union");
 		rdbtnUnion.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnUnion.setBounds(172, 95, 52, 23);
+		rdbtnUnion.setBounds(172, 114, 52, 23);
 		panel.add(rdbtnUnion);
 		
-		JRadioButton rdbtnParentalsoloParent = new JRadioButton("Special Leave for Women");
-		rdbtnParentalsoloParent.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnParentalsoloParent.setBounds(121, 116, 131, 23);
-		panel.add(rdbtnParentalsoloParent);
+		JRadioButton rdbtnBirthdayLeave = new JRadioButton("Birthday Leave");
+		rdbtnBirthdayLeave.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		rdbtnBirthdayLeave.setBounds(80, 114, 93, 23);
+		panel.add(rdbtnBirthdayLeave);
+		
+		JRadioButton rdbtnPaternity = new JRadioButton("Paternity");
+		rdbtnPaternity.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		rdbtnPaternity.setBounds(10, 114, 66, 23);
+		panel.add(rdbtnPaternity);
 		
 		JRadioButton rdbtnParentalsoloParent_1 = new JRadioButton("Parental /Solo Parent");
 		rdbtnParentalsoloParent_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		rdbtnParentalsoloParent_1.setBounds(10, 116, 117, 23);
+		rdbtnParentalsoloParent_1.setBounds(10, 135, 117, 23);
 		panel.add(rdbtnParentalsoloParent_1);
 		
-		JLabel lblEmployeeNo = new JLabel("Employee No.:");
-		lblEmployeeNo.setBounds(10, 145, 72, 14);
-		panel.add(lblEmployeeNo);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(105, 142, 180, 20);
-		panel.add(textField);
+		JRadioButton rdbtnParentalsoloParent = new JRadioButton("Special Leave for Women");
+		rdbtnParentalsoloParent.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		rdbtnParentalsoloParent.setBounds(131, 135, 131, 23);
+		panel.add(rdbtnParentalsoloParent);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(105, 164, 180, 20);
+		textField_1.setBounds(119, 161, 166, 20);
 		panel.add(textField_1);
 		
-		JLabel lblNewLabel_5 = new JLabel("Name:");
-		lblNewLabel_5.setBounds(10, 167, 46, 14);
-		panel.add(lblNewLabel_5);
+		JLabel lblEmployeeNo = new JLabel("Employee No.:");
+		lblEmployeeNo.setBounds(10, 164, 99, 14);
+		panel.add(lblEmployeeNo);
 		
-		JLabel lblNewLabel_6 = new JLabel("Campus & Office:");
-		lblNewLabel_6.setBounds(10, 188, 86, 14);
-		panel.add(lblNewLabel_6);
+		JLabel lblNewLabel_5 = new JLabel("Name:");
+		lblNewLabel_5.setBounds(10, 186, 46, 14);
+		panel.add(lblNewLabel_5);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(105, 185, 180, 20);
+		textField_2.setBounds(119, 183, 166, 20);
 		panel.add(textField_2);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(105, 206, 180, 22);
-		panel.add(textArea);
-		
-		JLabel lblNewLabel_7 = new JLabel("Specific Purpose:");
-		lblNewLabel_7.setBounds(10, 211, 85, 14);
-		panel.add(lblNewLabel_7);
-		
-		JLabel lblNewLabel_8 = new JLabel("Period Aplied");
-		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_8.setBounds(10, 247, 86, 14);
-		panel.add(lblNewLabel_8);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(36, 265, 77, 20);
+		textField_3.setBounds(119, 204, 166, 20);
 		panel.add(textField_3);
 		
-		JLabel lblNewLabel_9 = new JLabel("From");
-		lblNewLabel_9.setBounds(10, 268, 46, 14);
-		panel.add(lblNewLabel_9);
+		JLabel lblNewLabel_6 = new JLabel("Campus & Office:");
+		lblNewLabel_6.setBounds(10, 207, 99, 14);
+		panel.add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_10 = new JLabel("To");
-		lblNewLabel_10.setBounds(123, 268, 17, 14);
-		panel.add(lblNewLabel_10);
+		JLabel lblNewLabel_7 = new JLabel("Specific Purpose:");
+		lblNewLabel_7.setBounds(10, 230, 99, 14);
+		panel.add(lblNewLabel_7);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(119, 225, 166, 22);
+		panel.add(textArea);
+		
+		JLabel lblNewLabel_8 = new JLabel("Period Aplied");
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_8.setBounds(10, 266, 86, 14);
+		panel.add(lblNewLabel_8);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-		textField_4.setBounds(138, 265, 86, 20);
+		textField_4.setBounds(42, 284, 77, 20);
 		panel.add(textField_4);
+		
+		JLabel lblNewLabel_9 = new JLabel("From");
+		lblNewLabel_9.setBounds(10, 287, 46, 14);
+		panel.add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("To");
+		lblNewLabel_10.setBounds(123, 287, 17, 14);
+		panel.add(lblNewLabel_10);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
-		textField_5.setBounds(105, 288, 86, 20);
+		textField_5.setBounds(138, 284, 86, 20);
 		panel.add(textField_5);
 		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(120, 307, 86, 20);
+		panel.add(textField_6);
+		
 		JLabel lblNewLabel_11 = new JLabel("Total no. of Days:");
-		lblNewLabel_11.setBounds(10, 291, 93, 14);
+		lblNewLabel_11.setBounds(10, 310, 109, 14);
 		panel.add(lblNewLabel_11);
 		
 		JLabel lblNewLabel_12 = new JLabel("Means of Contact:");
 		lblNewLabel_12.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_12.setBounds(10, 313, 117, 14);
+		lblNewLabel_12.setBounds(10, 332, 117, 14);
 		panel.add(lblNewLabel_12);
-		
-		textField_6 = new JTextField();
-		textField_6.setBounds(105, 338, 180, 20);
-		panel.add(textField_6);
-		textField_6.setColumns(10);
 		
 		textField_7 = new JTextField();
 		textField_7.setColumns(10);
-		textField_7.setBounds(105, 361, 180, 20);
+		textField_7.setBounds(119, 357, 166, 20);
 		panel.add(textField_7);
 		
-		textField_8 = new JTextField();
-		textField_8.setColumns(10);
-		textField_8.setBounds(105, 384, 180, 20);
-		panel.add(textField_8);
-		
 		JLabel lblNewLabel_13 = new JLabel("Name:");
-		lblNewLabel_13.setBounds(10, 341, 46, 14);
+		lblNewLabel_13.setBounds(10, 360, 46, 14);
 		panel.add(lblNewLabel_13);
 		
 		JLabel lblNewLabel_13_1 = new JLabel("Address:");
-		lblNewLabel_13_1.setBounds(10, 364, 46, 14);
+		lblNewLabel_13_1.setBounds(10, 383, 86, 14);
 		panel.add(lblNewLabel_13_1);
 		
 		JLabel lblNewLabel_13_2 = new JLabel("Contact no.:");
-		lblNewLabel_13_2.setBounds(10, 387, 66, 14);
+		lblNewLabel_13_2.setBounds(10, 406, 86, 14);
 		panel.add(lblNewLabel_13_2);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(119, 403, 166, 20);
+		panel.add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(119, 380, 166, 20);
+		panel.add(textField_9);
 		
 		JPanel remarksType = new JPanel();
 		remarksType.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
