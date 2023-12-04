1f68ed0c407ed9143493d086596f92a88cbaf593
Alexene Tomate <alekzene.cs@gmail.com>
2023-12-04 10:31:04 +0800

    Create Connect.java

---
+++ src/ceu/Connect.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Scanner;
+
+class Connect {
+    public Connection getConnection() {
+        try {
+            // Load the JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // Establish a connection
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/orderingsystem", "root", "");
+
+            // Return the connection object
+            return connection;
+        } catch (Exception e) {
+            // Handle the exception appropriately
+            throw new RuntimeException("Error connecting to database", e);
+        }
+    }
+    void Connection(String query) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/orderingsystem", "root", "");
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.executeUpdate();
+            }
+
+            connection.commit();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error connecting to database: " + e.getMessage(), e);
+        }
+    }
+}
