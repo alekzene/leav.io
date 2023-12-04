44d0fcf2195a94450a9ebd9cf8748b077acc009c
Alexene Tomate <alekzene.cs@gmail.com>
2023-12-04 23:55:27 +0800

    Fix .classpath

--- src/ceu/Main.java
+++ src/ceu/Main.java
@@ -5,9 +5,6 @@ import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
-		UserDashboardFrame userDashboard = new UserDashboardFrame();
-		 
-
 	        SwingUtilities.invokeLater(() -> {
 	            LogInFrame logInFrame = new LogInFrame();
 	            logInFrame.setVisible(true);
