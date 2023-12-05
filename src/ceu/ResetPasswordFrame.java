898ba1af8899586ce3ca4cb4965c5a00ac87a9e3
Acnolawgia <152496464+Acnolawgia@users.noreply.github.com>
2023-12-06 03:24:17 +0800

    Update in ResetPasswordFrame
    change the oldpassword to imployee ID(numbers only)
    add Icon both to new password and confirm password to see the hide password

--- src/ceu/ResetPasswordFrame.java
+++ src/ceu/ResetPasswordFrame.java
@@ -11,10 +11,10 @@ import java.util.*;
 public class ResetPasswordFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
-    private JPasswordField oldPasswordField;
     private JPasswordField newPasswordField;
     private JPasswordField confirmNewPasswordField;
     private ArrayList<String> passwords = new ArrayList<>();
+    private JTextField EmployeeIdTextField;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -44,25 +44,68 @@ public class ResetPasswordFrame extends JFrame {
         backgroundPanel.setBounds(0, 0, 434, 261);
         contentPane.add(backgroundPanel);
         backgroundPanel.setLayout(null);
+        
+        //Icon
+        JLabel ConfirmPassViewIcon = new JLabel("New label");
+        ConfirmPassViewIcon.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		  // Toggle password visibility
+        		confirmNewPasswordField.setEchoChar((confirmNewPasswordField.getEchoChar() == 0) ? '\u2022' : (char) 0);
+        		ConfirmPassViewIcon.setVisible(true);
+		          
+        	}
+        });
+        ConfirmPassViewIcon.setIcon(new ImageIcon(ResetPasswordFrame.class.getResource("/images/icons8-eye-24.png")));
+        ConfirmPassViewIcon.setBounds(397, 129, 24, 22);
+        backgroundPanel.add(ConfirmPassViewIcon);
+        
+        //Icon
+        JLabel NewPassViewIcon = new JLabel("New label");
+        NewPassViewIcon.addMouseListener(new MouseAdapter() {
+        	@Override
+        	public void mouseClicked(MouseEvent e) {
+        		  // Toggle password visibility
+        		newPasswordField.setEchoChar((newPasswordField.getEchoChar() == 0) ? '\u2022' : (char) 0);
+		          NewPassViewIcon.setVisible(true);
+		          
+        	}
+        });
+        NewPassViewIcon.setIcon(new ImageIcon(ResetPasswordFrame.class.getResource("/images/icons8-eye-24.png")));
+        NewPassViewIcon.setBounds(397, 97, 24, 22);
+        backgroundPanel.add(NewPassViewIcon);
+        
+        EmployeeIdTextField = new JTextField();
+        EmployeeIdTextField.addKeyListener(new KeyAdapter() {
+      	  public void keyTyped(KeyEvent e) {
+                char inputChar = e.getKeyChar();
+                if (!Character.isDigit(inputChar) && inputChar != KeyEvent.VK_BACK_SPACE) {
+                    e.consume(); // Ignore non-numeric input
+                }
+            }
+        });
+        EmployeeIdTextField.setBounds(185, 65, 202, 19);
+        backgroundPanel.add(EmployeeIdTextField);
+        EmployeeIdTextField.setColumns(10);
 
         // RESET PASSWORD LABEL
         JLabel resetPasswordLabel = new JLabel("Reset Password");
-        resetPasswordLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-        resetPasswordLabel.setBounds(10, 11, 124, 14);
+        resetPasswordLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+        resetPasswordLabel.setBounds(20, 10, 145, 14);
         backgroundPanel.add(resetPasswordLabel);
 
-        // OLD PASSWORD LABEL
+        // Employee ID
         JLabel employee_Id = new JLabel("Employee ID:");
         employee_Id.setHorizontalAlignment(SwingConstants.CENTER);
         employee_Id.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        employee_Id.setBounds(20, 80, 155, 14);
+        employee_Id.setBounds(20, 65, 155, 14);
         backgroundPanel.add(employee_Id);
 
         // NEW PASSWORD LABEL
         JLabel newPasswordLabel = new JLabel("New Password:");
         newPasswordLabel.setHorizontalAlignment(SwingConstants.CENTER);
         newPasswordLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        newPasswordLabel.setBounds(10, 104, 165, 14);
+        newPasswordLabel.setBounds(10, 97, 165, 14);
         backgroundPanel.add(newPasswordLabel);
 
         // CONFIRM NEW PASSWORD LABEL
@@ -92,25 +135,15 @@ public class ResetPasswordFrame extends JFrame {
         cancelButton.setBounds(335, 179, 89, 23);
         backgroundPanel.add(cancelButton);
 
-        // OLD PASSWORD FIELD
-        oldPasswordField = new JPasswordField();
-        oldPasswordField.setBounds(185, 77, 239, 20);
-        backgroundPanel.add(oldPasswordField);
-
         // NEW PASSWORD FIELD
         newPasswordField = new JPasswordField();
-        newPasswordField.setBounds(185, 103, 239, 20);
+        newPasswordField.setBounds(185, 97, 202, 20);
         backgroundPanel.add(newPasswordField);
 
         // CONFIRM NEW PASSWORD FIELD
         confirmNewPasswordField = new JPasswordField();
-        confirmNewPasswordField.setBounds(185, 129, 239, 20);
+        confirmNewPasswordField.setBounds(185, 129, 202, 20);
         backgroundPanel.add(confirmNewPasswordField);
-        
-        JLabel userName = new JLabel("[insert username]");
-        userName.setFont(new Font("Tahoma", Font.BOLD, 14));
-        userName.setBounds(10, 47, 239, 14);
-        backgroundPanel.add(userName);
         
         JLabel lblNewLabel = new JLabel("");
         lblNewLabel.setIcon(new ImageIcon(ResetPasswordFrame.class.getResource("/images/bbg.png")));
@@ -123,33 +156,37 @@ public class ResetPasswordFrame extends JFrame {
         okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String oldPassword = new String(oldPasswordField.getPassword());
+            	String EmployeeID = new String(EmployeeIdTextField.getText());
                 String newPassword = new String(newPasswordField.getPassword());
                 String confirmNewPassword = new String(confirmNewPasswordField.getPassword());
 
-                if (!passwords.contains(oldPassword)) {
-                    JOptionPane.showMessageDialog(null, "Incorrect old password.");
+      
+//                if (oldPassword.equals(newPassword)) {
+//                    JOptionPane.showMessageDialog(null, "New password should be different from the old password.");
+//                    return;
+//                }
+
+                
+                // Check if newPassword is null or empty
+                if (newPassword == null || newPassword.trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Password cannot be empty.");
                     return;
                 }
-
-                if (oldPassword.equals(newPassword)) {
-                    JOptionPane.showMessageDialog(null, "New password should be different from the old password.");
-                    return;
-                }
-
+                
                 if (!newPassword.equals(confirmNewPassword)) {
                     JOptionPane.showMessageDialog(null, "New password and confirm password do not match.");
                     return;
                 }
-
-                updatePassword(oldPassword, newPassword);
+                updatePassword(newPassword);
                 JOptionPane.showMessageDialog(null, "Password updated successfully.");
             }
         });
     }
 
-    private void updatePassword(String oldPassword, String newPassword) {
-        passwords.remove(oldPassword);
+    private void updatePassword(String newPassword) {
+       
+        // For example, you might want to update the password in a database
+        // or store it in a data structure
         passwords.add(newPassword);
     }
 }
