ecd9d558bcca13c253f7153ad2f0f01f09463fd9
Alexene Tomate <alekzene.cs@gmail.com>
2023-12-05 17:51:52 +0800

    Update ResetPasswordFrame.java

--- src/ceu/ResetPasswordFrame.java
+++ src/ceu/ResetPasswordFrame.java
@@ -2,31 +2,18 @@ package ceu;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import java.awt.Color;
-import javax.swing.JPasswordField;
-
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
 
 public class ResetPasswordFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
-    private JPasswordField passwordField;
-    private JPasswordField passwordField_1;
-    private JPasswordField passwordField_2;
+    private JPasswordField oldPasswordField;
+    private JPasswordField newPasswordField;
+    private JPasswordField confirmNewPasswordField;
     private ArrayList<String> passwords = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -45,69 +32,80 @@ public class ResetPasswordFrame extends JFrame {
     public ResetPasswordFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 259);
+        
+        // CONTENT PANE
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JPanel BGpanel = new JPanel();
-        BGpanel.setBounds(0, 0, 434, 261);
-        contentPane.add(BGpanel);
-        BGpanel.setLayout(null);
+        // BACKGROUND PANEL
+        JPanel backgroundPanel = new JPanel();
+        backgroundPanel.setBounds(0, 0, 434, 261);
+        contentPane.add(backgroundPanel);
+        backgroundPanel.setLayout(null);
 
-        JLabel resetPassword = new JLabel("Reset Password");
-        resetPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
-        resetPassword.setBounds(10, 11, 124, 14);
-        BGpanel.add(resetPassword);
+        // RESET PASSWORD LABEL
+        JLabel resetPasswordLabel = new JLabel("Reset Password");
+        resetPasswordLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+        resetPasswordLabel.setBounds(10, 11, 124, 14);
+        backgroundPanel.add(resetPasswordLabel);
 
-        JLabel lblResetPassword = new JLabel("Old Password:");
-        lblResetPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        lblResetPassword.setBounds(10, 78, 165, 14);
-        BGpanel.add(lblResetPassword);
+        // OLD PASSWORD LABEL
+        JLabel oldPasswordLabel = new JLabel("Old Password:");
+        oldPasswordLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        oldPasswordLabel.setBounds(10, 78, 165, 14);
+        backgroundPanel.add(oldPasswordLabel);
 
-        JLabel lblNewPassword = new JLabel("New Password:");
-        lblNewPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        lblNewPassword.setBounds(10, 104, 165, 14);
-        BGpanel.add(lblNewPassword);
+        // NEW PASSWORD LABEL
+        JLabel newPasswordLabel = new JLabel("New Password:");
+        newPasswordLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        newPasswordLabel.setBounds(10, 104, 165, 14);
+        backgroundPanel.add(newPasswordLabel);
 
-        JLabel lblConfirmNewPassword = new JLabel("Confirm New Password:");
-        lblConfirmNewPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        lblConfirmNewPassword.setBounds(10, 130, 165, 14);
+        // CONFIRM NEW PASSWORD LABEL
+        JLabel confirmNewPasswordLabel = new JLabel("Confirm New Password:");
+        confirmNewPasswordLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        confirmNewPasswordLabel.setBounds(10, 130, 165, 14);
+        backgroundPanel.add(confirmNewPasswordLabel);
 
-        BGpanel.add(lblConfirmNewPassword);
+        // OK BUTTON
+        JButton okButton = new JButton("OK");
+        okButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+        okButton.setBackground(new Color(135, 206, 235));
+        okButton.setBounds(236, 179, 89, 23);
+        backgroundPanel.add(okButton);
 
-        JButton btnNewButton = new JButton("OK");
-        btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
-        btnNewButton.setBackground(new Color(135, 206, 235));
-        btnNewButton.setBounds(236, 179, 89, 23);
-        BGpanel.add(btnNewButton);
+        // CANCEL BUTTON
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        cancelButton.setBounds(335, 179, 89, 23);
+        backgroundPanel.add(cancelButton);
 
-        JButton btnCancel = new JButton("Cancel");
-        btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        btnCancel.setBounds(335, 179, 89, 23);
-        BGpanel.add(btnCancel);
+        // OLD PASSWORD FIELD
+        oldPasswordField = new JPasswordField();
+        oldPasswordField.setBounds(185, 77, 239, 20);
+        backgroundPanel.add(oldPasswordField);
 
-        passwordField = new JPasswordField();
-        passwordField.setBounds(185, 77, 239, 20);
-        BGpanel.add(passwordField);
+        // NEW PASSWORD FIELD
+        newPasswordField = new JPasswordField();
+        newPasswordField.setBounds(185, 103, 239, 20);
+        backgroundPanel.add(newPasswordField);
 
-        passwordField_1 = new JPasswordField();
-        passwordField_1.setBounds(185, 103, 239, 20);
-        BGpanel.add(passwordField_1);
+        // CONFIRM NEW PASSWORD FIELD
+        confirmNewPasswordField = new JPasswordField();
+        confirmNewPasswordField.setBounds(185, 129, 239, 20);
+        backgroundPanel.add(confirmNewPasswordField);
 
-        passwordField_2 = new JPasswordField();
-        passwordField_2.setBounds(185, 129, 239, 20);
-        BGpanel.add(passwordField_2);
-
+        // FIXME: LOGIC
         passwords.add("samplepass");
 
-        btnNewButton.addActionListener(new ActionListener() {
+        okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String oldPassword = new String(passwordField.getPassword());
-                String newPassword = new String(passwordField_1.getPassword());
-                String confirmNewPassword = new String(passwordField_2.getPassword());
+                String oldPassword = new String(oldPasswordField.getPassword());
+                String newPassword = new String(newPasswordField.getPassword());
+                String confirmNewPassword = new String(confirmNewPasswordField.getPassword());
 
                 if (!passwords.contains(oldPassword)) {
                     JOptionPane.showMessageDialog(null, "Incorrect old password.");
@@ -129,7 +127,7 @@ public class ResetPasswordFrame extends JFrame {
             }
         });
 
-        btnCancel.addActionListener(new ActionListener() {
+        cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Add code for cancel button action, if needed
