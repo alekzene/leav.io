db569164dba80a6ae589fd6dd44ad1e2cf82da6d
Zenkhria <118682206+Zenkhria@users.noreply.github.com>
2023-12-08 21:09:08 +0800

    Removed Calendar

--- src/ceu/LeaveApplicationFormFrame2.java
+++ src/ceu/LeaveApplicationFormFrame2.java
@@ -250,19 +250,19 @@ public class LeaveApplicationFormFrame2 extends JFrame {
         deptLabel.setBounds(10, 267, 99, 25);
         formPanel.add(deptLabel);
         
-//        departmentSelect = new JComboBox();
-//        departmentSelect.setModel(new DefaultComboBoxModel(new String[] {"Select Department", "Canteen", "Library", "Faculty", "Maintenance\t", "Accounting Office", "HR"}));
-//        departmentSelect.setFont(new Font("Tahoma", Font.PLAIN, 12));
-//        departmentSelect.setBackground(new Color(255, 255, 255));
-//        departmentSelect.setBounds(132, 270, 239, 21);
-//        formPanel.add(departmentSelect);
-//        
-//        leaveTypeSelect = new JComboBox();
-//        leaveTypeSelect.addActionListener(new ActionListener() {
-//        	public void actionPerformed(ActionEvent e) {
-//        		updateDateRestrictions();
-//        	}
-//        });
+        departmentSelect = new JComboBox();
+        departmentSelect.setModel(new DefaultComboBoxModel(new String[] {"Select Department", "Canteen", "Library", "Faculty", "Maintenance\t", "Accounting Office", "HR"}));
+        departmentSelect.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        departmentSelect.setBackground(new Color(255, 255, 255));
+        departmentSelect.setBounds(132, 270, 239, 21);
+        formPanel.add(departmentSelect);
+        
+        leaveTypeSelect = new JComboBox();
+        leaveTypeSelect.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		updateDateRestrictions();
+        	}
+        });
         
        
         leaveTypeSelect.setModel(new DefaultComboBoxModel(new String[] {"Select Type", "Sick Leave", "Emergency Leave", "Vacation Leave", "Maternity Leave", "Paternity Leave"}));
@@ -348,48 +348,48 @@ public class LeaveApplicationFormFrame2 extends JFrame {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	private static String getCurrentDate() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return dateFormat.format(new Date());
-    }
-   
-    private void refreshCalendar(int month, int year) {
-        //Variables
-        String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        int nod, som; //Number Of Days, Start Of Month
-        
-        //Allow/disallow buttons
-        btnPrev.setEnabled(true);
-        btnNext.setEnabled(true);
-        if (month == 0 && year <= realYear-10){btnPrev.setEnabled(false);} //Too early
-        if (month == 11 && year >= realYear+100){btnNext.setEnabled(false);} //Too late
-        lblMonth.setText(months[month]); //Refresh the month label (at the top)
-        lblMonth.setBounds(160-lblMonth.getPreferredSize().width/2, 25, 180, 25); //Re-align label with calendar
-        cmbYear.setSelectedItem(String.valueOf(year)); //Select the correct year in the combo box
-        
-        //Clear table
-        for (int i=0; i<6; i++){
-            for (int j=0; j<7; j++){
-                mtblCalendar.setValueAt(null, i, j);
-            }
-        }
-        
-        //Get first day of month and number of days
-        GregorianCalendar cal = new GregorianCalendar(year, month, 1);
-        nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-        som = cal.get(GregorianCalendar.DAY_OF_WEEK);
-        
-        //Draw calendar
-        for (int i=1; i<=nod; i++){
-            int row = new Integer((i+som-2)/7);
-            int column  =  (i+som-2)%7;
-            mtblCalendar.setValueAt(i, row, column);
-        }
-        
-        //Apply renderers
-        tblCalendar.setDefaultRenderer(tblCalendar.getColumnClass(0), new tblCalendarRenderer());
-    }
+//
+//	private static String getCurrentDate() {
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+////        return dateFormat.format(new Date());
+////    }
+//   
+//    private void refreshCalendar(int month, int year) {
+//        //Variables
+//        String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+//        int nod, som; //Number Of Days, Start Of Month
+//        
+//        //Allow/disallow buttons
+//        btnPrev.setEnabled(true);
+//        btnNext.setEnabled(true);
+//        if (month == 0 && year <= realYear-10){btnPrev.setEnabled(false);} //Too early
+//        if (month == 11 && year >= realYear+100){btnNext.setEnabled(false);} //Too late
+//        lblMonth.setText(months[month]); //Refresh the month label (at the top)
+//        lblMonth.setBounds(160-lblMonth.getPreferredSize().width/2, 25, 180, 25); //Re-align label with calendar
+//        cmbYear.setSelectedItem(String.valueOf(year)); //Select the correct year in the combo box
+//        
+//        //Clear table
+//        for (int i=0; i<6; i++){
+//            for (int j=0; j<7; j++){
+//                mtblCalendar.setValueAt(null, i, j);
+//            }
+//        }
+//        
+//        //Get first day of month and number of days
+//        GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+//        nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+//        som = cal.get(GregorianCalendar.DAY_OF_WEEK);
+//        
+//        //Draw calendar
+//        for (int i=1; i<=nod; i++){
+//            int row = new Integer((i+som-2)/7);
+//            int column  =  (i+som-2)%7;
+//            mtblCalendar.setValueAt(i, row, column);
+//        }
+//        
+//        //Apply renderers
+//        tblCalendar.setDefaultRenderer(tblCalendar.getColumnClass(0), new tblCalendarRenderer());
+    
     
     private void updateDateRestrictions() {
         String selectedLeaveType = (String) leaveTypeSelect.getSelectedItem();
